b4fc1a0da9a2bb1e57eeb4d65079398edbecf9da
Paul Schaaf <paulsimonschaaf@gmail.com>
2022-07-02 15:32:13 +0200

    initial progress bar - using setInterval instead off setTimeout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
-  <input v-model="inputLetter" ref="input" type="text" style="opacity: 0" />
-  <div style="display: flex; justify-content: center">
-    <div style="border: 5px solid orange; border-radius: 8px; width: 300px">
+  <input
+    v-model="inputLetter"
+    ref="input"
+    type="text"
+    style="opacity: 0; position: absolute"
+  />
+  <div
+    style="
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    "
+  >
+    <div
+      style="
+        border: 5px solid orange;
+        border-radius: 8px;
+        width: 300px;
+        padding: 50px 0 50px 0;
+      "
+    >
       <p style="font-size: 80px">{{ letter }}</p>
+    </div>
+    <div
+      style="
+        width: 295px;
+        margin-top: 10px;
+        height: 15px;
+        border-radius: 50px;
+        border: 1px solid transparent;
+        overflow-x: hidden;
+        overflow-y: hidden;
+      "
+    >
+      <div
+        id="container"
+        style="width: 300px; transform: translate(-2.5px)"
+      ></div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, Ref, ref } from "vue";
+import * as ProgressBar from "progressbar.js";
 
 export default defineComponent({
   name: "App",
@@ -41,6 +78,18 @@ export default defineComponent({
       "y",
       "z",
     ];
+    const createProgressBar = () => {
+      const bar = new ProgressBar.Line("#container", {
+        strokeWidth: 4,
+        easing: "linear",
+        duration: 2000,
+        color: "blue",
+        trailColor: "#eee",
+        svgStyle: { width: "100%", height: "100%" },
+      });
+      bar.animate(1.0);
+      return bar;
+    };
     let counter = 0;
     const getLetter = () => {
       // eslint-disable-next-line no-constant-condition
@@ -54,21 +103,25 @@ export default defineComponent({
     const letter = ref("") as Ref<string | number>;
     const inputLetter = ref("");
     const input = ref(null as unknown as HTMLElement);
-
+    let bar: any = null;
     onMounted(() => {
       letter.value = getLetter();
       input.value.focus();
       const letterInterval = setInterval(() => {
+        bar.destroy();
         if (letter.value === inputLetter.value) {
           letter.value = getLetter();
           input.value.focus();
           inputLetter.value = "";
           counter += 1;
+          bar = createProgressBar();
         } else {
           clearInterval(letterInterval);
           letter.value = counter;
         }
       }, 2000);
+
+      bar = createProgressBar();
     });
 
     return { letter, input, inputLetter };
@@ -77,6 +130,11 @@ export default defineComponent({
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
